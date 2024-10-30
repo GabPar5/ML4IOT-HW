dd9a82380aded523e729dcd638cf41d75edd2097
GabPar5 <31355000+GabPar5@users.noreply.github.com>
2024-10-30 12:23:03 +0100

    Renamed exercise 1 and added constants for better usability

--- HW_1/ex1.py
+++ HW_1/ex1.py
@@ -1,32 +1,36 @@
 import redis
 from time import time, sleep
-import uuid
 from datetime import datetime
+import uuid
 import adafruit_dht
 from board import D4
 import argparse
 
 
+# Parse arguments passed when running this script through the terminal
 parser = argparse.ArgumentParser()
 parser.add_argument("-ho", "--host", default = None, type = str, help="Redis Cloud Host")
 parser.add_argument("-po", "--port", default = None, type = int, help="Redis Cloud Port")
 parser.add_argument("-us", "--user", default = None, type = str, help="Redis Cloud Username")
 parser.add_argument("-pw", "--password", default = None, type = str, help="Redis Cloud Password")
-
 params = parser.parse_args()
 
-# Implementation of LOSSLESS and LOSSY compression methods
-mac_address = hex(uuid.getnode()) # Get the MAC address of the Raspberry PI
-dht_device = adafruit_dht.DHT11(D4) # Declare the existence of the humidity-temperature sensor and indicate the linking pin! (D4)
-
-
-# Database parameters
+# Parameters and constants
 REDIS_HOST = params.host
 REDIS_PORT = params.port
 REDIS_USERNAME = params.user
 REDIS_PASSWORD = params.password
+ONE_DAY_IN_MS = 24*60*60*1000
+RETENTION_DAYS = 30
+RETENTION_DAYS_AGG = 365
+SLEEP_SECS = 2
+BUCKET_SIZE_MINS = 60
 
-# Establish a connection to the database and check if the connection works
+# Get the MAC address of the Raspberry PI and identify the humidity-temperature sensor
+mac_address = hex(uuid.getnode()) 
+dht_device = adafruit_dht.DHT11(D4)
+
+# Establish a connection to the database and check if it works
 redis_client = redis.Redis(host = REDIS_HOST, 
                            port = REDIS_PORT, 
                            username = REDIS_USERNAME, 
@@ -45,16 +49,17 @@ try:
 except:
     pass
 
-# Set the retention of the 'temperature' and 'humidity' time series to 30 days
-one_day_in_ms = 24*60*60*1000
-redis_client.ts().alter(mac_address + ':temperature', retention_msecs = one_day_in_ms*30)
-redis_client.ts().alter(mac_address + ':humidity', retention_msecs = one_day_in_ms*30)
+# Set the retention of the 'temperature' and 'humidity' time series to "RETENTION_DAYS" days (in this case 30)
+redis_client.ts().alter(mac_address + ':temperature', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS)
+redis_client.ts().alter(mac_address + ':humidity', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS)
 
 try: 
-    while True: # Record the temperature and humidity every 2 seconds until the key 'q' is pressed
+    # Record the temperature and humidity every "SLEEP_SECS" seconds (in this case 2) until the key "CTRL^C" is pressed
+    while True: 
         
+        # Get the timestamp in ms and convert it to human-readable time
         timestamp_ms = int(time()*1000)
-        formatted_time = datetime.fromtimestamp(timestamp_ms/1000).strftime('%Y-%m-%d %H:%M:%S.%f') # Convert the timestamp to human-readable time
+        formatted_time = datetime.fromtimestamp(timestamp_ms/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
 
         try:
             # Get temperature and humidity from the sensor, store them in the database and print them
@@ -73,59 +78,60 @@ try:
             dht_device.exit()
             dht_device = adafruit_dht.DHT11(D4)
 
-        sleep(2) # Check the temperature and humidity every 2 seconds
+        sleep(SLEEP_SECS)
 except KeyboardInterrupt:
     pass
 
 
-
-# Create aggregated time series for temperature and humidity
+# Create aggregated time series for temperature and humidity,
+# calculating them every "BUCKET_MINS" minutes (in this case 60).
+# Set their retention period to "RETENTION_DAYS_AGG" days (in this case 365)
 # avg
 print(f"calculating metrics...")
 try:
     redis_client.ts().create(mac_address + ':temperature_avg')
-    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_avg', 'avg', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':temperature_avg', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_avg', 'avg', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':temperature_avg', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 
 except redis.ResponseError:
     pass
 
 try:
     redis_client.ts().create(mac_address + ':humidity_avg')
-    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_avg', 'avg', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':humidity_avg', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_avg', 'avg', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':humidity_avg', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 except redis.ResponseError:
     pass
 
 # min
 try:
     redis_client.ts().create(mac_address + ':temperature_min')
-    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_min', 'min', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':temperature_min', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_min', 'min', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':temperature_min', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 
 except redis.ResponseError:
     pass
 
 try:
     redis_client.ts().create(mac_address + ':humidity_min')
-    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_min','min', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':humidity_min', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_min','min', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':humidity_min', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 except redis.ResponseError:
     pass
 
 # max
 try:
     redis_client.ts().create(mac_address + ':temperature_max')
-    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_max', 'max', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':temperature_max', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':temperature',mac_address + ':temperature_max', 'max', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':temperature_max', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 
 except redis.ResponseError:
     pass
 
 try:
     redis_client.ts().create(mac_address + ':humidity_max')
-    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_max','max', bucket_size_msec=1000*60*60)
-    redis_client.ts().alter(mac_address + ':humidity_max', retention_msecs = one_day_in_ms*365)
+    redis_client.ts().createrule(mac_address + ':humidity',mac_address + ':humidity_max','max', bucket_size_msec=1000*60*BUCKET_SIZE_MINS)
+    redis_client.ts().alter(mac_address + ':humidity_max', retention_msecs = ONE_DAY_IN_MS*RETENTION_DAYS_AGG)
 except redis.ResponseError:
     pass
 
